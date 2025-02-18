f4a8e6e50c7c3268b7b7a4199a4bd493eae2a317
ybelur <yash.belur1@icloud.com>
2025-02-18 09:16:53 +0000

    updated figures

--- Federated_Averaging_Manual/plot/plot_no_attack.py
+++ Federated_Averaging_Manual/plot/plot_no_attack.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the data
-file_path = "../results/no_attack_results.csv"
+file_path = "../results/no_attack_results_10_clients.csv"
 data = pd.read_csv(file_path)
 
 # Plot the data
@@ -23,7 +23,7 @@ plt.title("Global Accuracy vs Number of Rounds")
 plt.xlabel("Number of Rounds")
 plt.ylabel("Global Accuracy")
 plt.legend(title="Number of Epochs per Round", bbox_to_anchor=(1, 1), loc='upper left')
-plt.savefig("../figures/no_attack_plot_10_clients.png", bbox_inches='tight')
+plt.savefig("../figures/no_attack_plot_10_clients.png", bbox_inches='tight', dpi=300)
 
 # Calculate the moving average
 window = 10
@@ -44,4 +44,4 @@ plt.title("Moving Average of Global Accuracy vs Number of Rounds")
 plt.xlabel("Number of Rounds")
 plt.ylabel("Moving Average of Global Accuracy")
 plt.legend(title="Number of Epochs per Round", bbox_to_anchor=(1, 1), loc='upper left')
-plt.savefig("../figures/no_attack_moving_average_plot_10_clients.png", bbox_inches='tight')
+plt.savefig("../figures/no_attack_moving_average_plot_10_clients.png", bbox_inches='tight', dpi=300)
