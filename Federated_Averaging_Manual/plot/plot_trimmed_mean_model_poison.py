287784289321b0736948935d5a5e9c6497058989
ybelur <yash.belur1@icloud.com>
2025-02-28 18:41:15 +0000

    new figures

--- Federated_Averaging_Manual/plot/plot_trimmed_mean_model_poison.py
+++ Federated_Averaging_Manual/plot/plot_trimmed_mean_model_poison.py
@@ -3,29 +3,30 @@ import pandas as pd
 import seaborn as sns
 
 # Load the data
-file_path = "../results/median_data_poison_10_clients.csv"
+file_path = "../results/trimmed_mean_model_poison_10_clients.csv"
 data = pd.read_csv(file_path)
 
-data['Key Label'] = data['Number of Data Poisoned Clients'].apply(lambda x: f"{x} (No Data Poisoning)" if x == 0 else str(x))
+data['Key Label'] = data['Number of Model Poisoned Clients'].apply(lambda x: f"{x} (No Model Poisoning)" if x == 0 else str(x))
 
 # Set the plot style
 sns.set(style="whitegrid")
 
 # plt.figure(figsize=(12, 6))
 # sns.lineplot(
-#     data=data, 
-#     x="Round", 
-#     y="Global Accuracy", 
-#     hue="Key Label", 
+#     data=data,
+#     x="Round",
+#     y="Global Accuracy",
+#     hue="Key Label",
 #     marker="o",
 #     palette="bright"
 # )
 
-# plt.title("Global Accuracy vs Number of Rounds for Different Levels of Data Poisoning (Median Averaging)")
+# plt.title("Global Accuracy vs Number of Rounds for Different Levels of Model Poisoning (Trimmed Mean Averaging)")
 # plt.xlabel("Number of Rounds")
 # plt.ylabel("Global Accuracy")
 # plt.legend(title="Number of Poisoned Nodes", bbox_to_anchor=(1, 1), loc='upper left')
 # plt.tight_layout()
+# # plt.savefig("../figures/trimmed_mean_model_poison_10_clients.png", dpi=300)
 # plt.show()
 
 # Calculate the moving average
@@ -36,18 +37,18 @@ data['Moving Average'] = data.groupby('Key Label')['Global Accuracy'].transform(
 # Plot the moving average
 plt.figure(figsize=(12, 6))
 sns.lineplot(
-    data=data, 
-    x="Round", 
-    y="Moving Average", 
-    hue="Key Label", 
+    data=data,
+    x="Round",
+    y="Moving Average",
+    hue="Key Label",
     marker="o",
     palette="bright"
 )
 
-plt.title("Moving Average of Global Accuracy vs Number of Rounds for Different Levels of Data Poisoning (Median Averaging)")
+plt.title("Moving Average of Global Accuracy vs Number of Rounds for Different Levels of Model Poisoning (Trimmed Mean Averaging)")
 plt.xlabel("Number of Rounds")
 plt.ylabel("Moving Average of Global Accuracy")
 plt.legend(title="Number of Poisoned Nodes", bbox_to_anchor=(1, 1), loc='upper left')
 plt.tight_layout()
-plt.savefig("../figures/median_moving_avg_data_poison_10_clients.png", dpi=300)
-plt.show()
+plt.savefig("../figures/trimmed_mean_moving_avg_model_poison_10_clients.png", dpi=300)
+# plt.show()
