5c281f74494eb6017778f4d68b9e600145264236
ybelur <yash.belur1@icloud.com>
2025-03-01 13:13:41 +0000

    new plots for different trimmed mean ratio

--- Federated_Averaging_Manual/plot/plot_trimmed_mean_model_poison.py
+++ Federated_Averaging_Manual/plot/plot_trimmed_mean_model_poison.py
@@ -3,7 +3,7 @@ import pandas as pd
 import seaborn as sns
 
 # Load the data
-file_path = "../results/trimmed_mean_model_poison_10_clients.csv"
+file_path = "../results/trimmed_mean_model_poison_10_clients_0.2.csv"
 data = pd.read_csv(file_path)
 
 data['Key Label'] = data['Number of Model Poisoned Clients'].apply(lambda x: f"{x} (No Model Poisoning)" if x == 0 else str(x))
@@ -50,5 +50,5 @@ plt.xlabel("Number of Rounds")
 plt.ylabel("Moving Average of Global Accuracy")
 plt.legend(title="Number of Poisoned Nodes", bbox_to_anchor=(1, 1), loc='upper left')
 plt.tight_layout()
-plt.savefig("../figures/trimmed_mean_moving_avg_model_poison_10_clients.png", dpi=300)
+plt.savefig("../figures/trimmed_mean_moving_avg_model_poison_10_clients_0.2.png", dpi=300)
 # plt.show()
