6f0c73221d0cafd28623a3cf972a6b68b8384170
ybelur <yash.belur1@icloud.com>
2025-05-12 21:08:38 +0100

    updated threshold added new figures:

--- Federated_Averaging_Manual/results/remove_prob.py
+++ Federated_Averaging_Manual/results/remove_prob.py
@@ -1,12 +1,12 @@
 import os
 
 # Path to your folder
-folder_path = "weighted_mean_model_poison_batch/"
+folder_path = "dropout_mean_0.3_prob_data_poison_batch/"
 
 # Loop through each file in the folder
 for filename in os.listdir(folder_path):
-    if "prob_" in filename:
-        new_name = filename.replace("prob_", "")
+    if "0.3_prob_" in filename:
+        new_name = filename.replace("0.3_prob_", "")
         old_path = os.path.join(folder_path, filename)
         new_path = os.path.join(folder_path, new_name)
         os.rename(old_path, new_path)
